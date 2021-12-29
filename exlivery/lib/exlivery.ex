36c8158009f27245d4f8dd49559c60d68679b9d7
Joabe Henrique <henriquecidoz@hotmail.com>
2021-12-29 17:17:58 -0300

    feat: adicionando o modulo create_or_update para facilitar na criacao do usuario

--- exlivery/lib/exlivery.ex
+++ exlivery/lib/exlivery.ex
@@ -1,5 +1,8 @@
 defmodule Exlivery do
-  def hello do
-    :world
-  end
+  alias Exlivery.Users.Agent
+  alias Exlivery.Users.CreateOrUpdate
+
+  def start_agents, do: Agent.start_link()
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
 end
---
+++ exlivery/lib/users/create_or_update.ex
@@ -0,0 +1,13 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users.Agent
+  alias Exlivery.Users.User
+
+  def call(%{name: name, email: email, age: age, cpf: cpf, address: address}) do
+    name
+    |> User.build(email, age, cpf, address)
+    |> create_user()
+  end
+
+  def create_user({:ok, user}), do: Agent.increment_user(user)
+  def create_user({:error, _user}), do: {:error, "Entrada Inválida"}
+end
