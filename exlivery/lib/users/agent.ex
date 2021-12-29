5f2d3d4b973aad2f5b613f3c444fcea5055dc723
Joabe Henrique <henriquecidoz@hotmail.com>
2021-12-29 15:22:19 -0300

    feat: adicionando Agent para trabalhar em cima da criacao do usuario

---
+++ exlivery/lib/users/agent.ex
@@ -0,0 +1,24 @@
+defmodule Exlivery.Users.Agent do
+  use Agent
+
+  alias Exlivery.Users.User
+
+  def start_link() do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def increment_user(%User{} = user),
+    do: Agent.update(__MODULE__, fn agent_state -> increment(agent_state, user) end)
+
+  def get_user(cpf),
+    do: Agent.get(__MODULE__, fn agent_state -> get_user_cpf(agent_state, cpf) end)
+
+  defp increment(agent_state, %User{cpf: cpf} = user), do: Map.put(agent_state, cpf, user)
+
+  defp get_user_cpf(agent_state, cpf) do
+    case Map.get(agent_state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+end
