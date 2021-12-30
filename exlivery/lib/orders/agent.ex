6b3f3eb8a162e35052685603b02add65ff7064fe
Joabe Henrique <henriquecidoz@hotmail.com>
2021-12-30 11:16:24 -0300

    feat: adicionando Agent para trabalhar em cima da criacao do pedido

---
+++ exlivery/lib/orders/agent.ex
@@ -0,0 +1,23 @@
+defmodule Exlivery.Orders.Agent do
+  use Agent
+
+  alias Exlivery.Orders.Order
+
+  def start_link(), do: Agent.start_link(fn -> %{} end, name: __MODULE__)
+
+  def increment_order(%Order{} = order) do
+    uuid = UUID.uuid4()
+    Agent.update(__MODULE__, fn agent_state -> Map.put(agent_state, uuid, order) end)
+    {:ok, uuid}
+  end
+
+  def get_order(uuid),
+    do: Agent.get(__MODULE__, fn agent_state -> get_order_uuid(agent_state, uuid) end)
+
+  defp get_order_uuid(agent_state, uuid) do
+    case Map.get(agent_state, uuid) do
+      nil -> {:error, "Order not found"}
+      order -> {:ok, order}
+    end
+  end
+end
